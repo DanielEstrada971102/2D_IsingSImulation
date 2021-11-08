630912b4b01294d3975007124ff3d4378c1db05c
daniel <daniel.estrada1@udea.edu.co>
2021-11-08 12:41:15 -0500

    el programa run ya puede correrse en paralelo con la opcióón -p

--- run.py
+++ run.py
@@ -3,8 +3,8 @@
 * @author Daniel Estrada (daniel.estrada1@udea.edu.co)                         *
 * @brief Runs the 2D ising  model simulation, generates the files with the     *
 *        data for compute magnetization and the system evolution visualization *
-* @version 0.2                                                                 *
-* @date 2021-11-8                                                             *                                     
+* @version 0.1                                                                 *
+* @date 2021-10-14                                                             *                                     
 *                                                                              *
 * @copyright Copyright (c) 2021                                                *
 ********************************************************************************
@@ -13,6 +13,66 @@ from os import system as sys
 from numpy import arange
 from tqdm import tqdm
 from ising_classes import * # importing the classes for the simulation
+from multiprocessing import Process
+
+
+def run_simulation(b, N, L, m, folder = ".", init_cold = True, vb=True):
+    """
+    This function generates N updates of a LxL system for extract the values 
+    of magnetization in every step for differents temperatures "betas" and 
+    saves the data in "folder". 
+
+    @Parameters
+    -----------
+    ->  betas : float list or array/arange : numpy
+        ... list with the temperature variation for the simulation
+    ->  N : int
+        ... sample size to compute the monte carlo estimator
+    ->  L : int
+        ... length of the particles lattice
+    ->  m : int
+        ... number of Metropolis-hasting algotihm repetitions
+    ->  folder : str
+        ... directory path to save the output files
+    ->  init_cold : bool
+        ... if true, the sistem will start in a cold state (every spins up (1))
+    ->  vb : bool
+        ... verbose argument
+    @Retuns
+    -------
+    -
+    """
+
+    if vb : print("runing for L = %.d"%L)
+
+
+    #path to save the spins configuration file and visualize the sytem evolution
+    spinConfigFiles_path = folder + "/visual/b_%.3f"%b
+    #path to save the magnetization and energy data
+    dataFiles_path = folder + "/data/b%.3f.txt"%b
+
+    sys("mkdir " + spinConfigFiles_path) #creating the folder
+
+    with open(dataFiles_path, 'w') as f:
+        
+        f.write("M\tE\n") #file header 
+        if vb: print("runing for beta = %.3f"%b)
+        
+        system = IsingLattice(L, b) # the particles sysyem is initialized
+        
+        # initial magnetization and energy values are written
+        f.write("%.4f\t%.4f\n"%(system.M, system.E)) 
+        # initial spins configuration is saved
+        gen_visualFile(spinConfigFiles_path + "/step0.txt"%b, system)
+
+        for n in tqdm(range(1, N), disable = not vb):         
+            # a new spins configuration is genereted with Metropolis algorithm          
+            system.updateLattice(m)            
+            # new system spins configuration is saved
+            gen_visualFile(spinConfigFiles_path + "/step%d.txt"%(n), system)
+            # new magnetization and energy values are written
+            f.write("%.4f\t%.4f\n"%(system.M, system.E))
+
 
 def gen_visualFile(filename, system):
     """
@@ -34,46 +94,29 @@ def gen_visualFile(filename, system):
             f.write(''.join(line)+"\n")
 
 
-def main(bmin, bmax, bstep, N, l, m, attempt='', cold=True, verbose=True):   
+def main(bmin, bmax, bstep, N, l, m, attempt='', cold=True, verbose=True, 
+         parallel=False):   
     # the container folders are created
-    folder = "L" + attempt + "_%d"%l
-    sys("mkdir " + folder)
-    sys("mkdir " + folder+"/data " + folder + "/visual")
+    folderName = "L" + attempt + "_%d"%l
+    sys("mkdir " + folderName)
+    sys("mkdir " + folderName+"/data " + folderName + "/visual")
 
     betas = arange(bmin, bmax, bstep)
+
+    # the simulation is runed
+    if parallel:
+        processes = []
+        for b in betas:
+            processes.append(Process(target=run_simulation, 
+                            args=[b,N, l, m, folderName, cold, verbose]))
+            processes[-1].start()
+        
+        for process in processes:
+            process.join()
     
-    # the simulation is developed
-    
-    if verbose : print("runing for L = %.d"%l)
-
-    for b in betas:
-        #path to save the spins configuration file and visualize the sytem evolution
-        spinConfigFiles_path = folder + "/visual/b_%.3f"%b
-        #path to save the magnetization and energy data
-        dataFiles_path = folder + "/data/b%.3f.txt"%b
-
-        sys("mkdir " + spinConfigFiles_path) #creating the folder
-
-        with open(dataFiles_path, 'w') as f:
-            
-            f.write("M\tE\n") #file header 
-            if verbose: print("runing for beta = %.3f"%b)
-            
-            system = IsingLattice(l, b) # the particles sysyem is initialized
-            
-            # initial magnetization and energy values are written
-            f.write("%.4f\t%.4f\n"%(system.M, system.E)) 
-            # initial spins configuration is saved
-            gen_visualFile(spinConfigFiles_path + "/step0.txt"%b, system)
-
-            for n in tqdm(range(1, N), disable = not verbose):         
-                # a new spins configuration is genereted with Metropolis algorithm          
-                system.updateLattice(m)
-
-                # new system spins configuration is saved
-                gen_visualFile(spinConfigFiles_path + "/step%d.txt"%(n), system)
-                # new magnetization and energy values are written
-                f.write("%.4f\t%.4f\n"%(system.M, system.E))
+    else:
+        for b in betas:
+            run_simulation(b, N, l, m, folderName, cold, verbose)
 
 
 if __name__ == "__main__":
@@ -101,8 +144,10 @@ if __name__ == "__main__":
                           necessary to specify the number of the attempt')
     parser.add_argument('-nv', type=int, metavar='', default=0, choices=[0,1],
                     help='verbose disable')
+    parser.add_argument('-p', type=int, metavar='', default=0, choices=[0,1],
+                    help='run in parallel')
 
     args = parser.parse_args()
     # main execution...........
     main(args.bmin, args.bmax, args.bstep, args.N, args.L, 
-        args.m, args.attmp, args.cold, not args.nv)
+        args.m, args.attmp, args.cold, not args.nv, args.p)
